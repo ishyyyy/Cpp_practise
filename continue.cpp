1704a89e73c4d97d20cf4cc4b9cf5ad2fbbbd56e
Isha Sharma <86165115+ishyyyy@users.noreply.github.com>
2022-08-12 07:56:38 +0530

    Add files via upload

---
+++ continue.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int n;
+    cout<<"ENTER THE VALUE OF N :"<<endl;
+    cin>>n;
+    for(i=0 ; i<=n ; ++i)
+    {
+   
+    if(i==4)
+    {
+      continue;
+    }
+     cout<<"Ishu Loves Isha";
+    }
+    return 0;
+}
